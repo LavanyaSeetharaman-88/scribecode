2f1b17bdc7cdd7617d31423abe7f13b9a42478e7
LearnersCode-Creator <lavanya.seetharaman20@gmail.com>
2019-11-26 22:52:17 +0530

    3 files modified

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -10,8 +10,25 @@ export class MenuComponent implements OnInit {
   lastName:String="Creators";
   age:number=10;
   logo:string="https://angular.io/assets/images/logos/angular/angular.svg";
+  width:number=50;
+  height:number=100;
+  mytext:string="angular"
   constructor() { }
 
+  sayHello(){
+    alert("this is my angular project using stackblitz");
+  }
+  textChanged(event){
+    console.log(event.target.value);
+  }
+  textChangeFn(){
+    if(this.mytext=="angular"){
+        this.mytext="learnersCode";
+    }else{     
+        this.mytext="angular";
+    }
+    
+  }
   ngOnInit() {
   }
 
